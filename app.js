53d26a3fb0471345d7142ea1fd1748a639bc88d2
Rikky <rozsarichardzon@gmail.com>
2025-10-03 10:27:09 +0200

    salt

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as db from "./util/database.js";
+import bcrypt from "bcrypt";
 
 const PORT = 3000;
 const app = express();
@@ -7,41 +8,42 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-
-app.post("/register", (req,res) =>{
-    const {email, password} = req.body;
-    if (!email || !password) {
-        return res.status(400).json({message: "Missing some data"});
-    }
-    const user = db.getUserByMail(email);
-    if (!user){
-        db.saveUser(email,password);
-        return res.status(200).json({message: "User created"});
-    }
-    return res.status(400).json({message:"Ez az email már foglalt."});
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Missing some data" });
+  }
+  const user = db.getUserByMail(email);
+  if (!user) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    db.saveUser(email, hashedPassword);
+    return res.status(200).json({ message: "User created" });
+  }
+  return res.status(400).json({ message: "Ez az email már foglalt." });
 });
 
-app.post("/singin", (req, res) => {
-    try {
-        const {email, password} = req.body;
+app.post("/singin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
     if (!email || !password) {
-        return res.status(400).json({message: "Missing some data"});
+      return res.status(400).json({ message: "Missing some data" });
     }
     const user = db.getUserByMail(email);
-    if (!user){
-        return res.status(400).json({message:"Email vagy jelszó nem egyezik!1"});
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Email vagy jelszó nem egyezik!1" });
     }
-    if (user.password === password) {
+    if (bcrypt.compareSync(password, user.password)) {
       return res.status(200).json({ message: "Sikeres bejelentkezés!" });
     }
-    return res.status(400).json({message:"Email vagy jelszó nem egyezik!"});
-    }
-    catch (error){
-        console.log(error)
-    }
+    return res.status(400).json({ message: "Email vagy jelszó nem egyezik!" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.listen(PORT, ()=> {
-    console.log("http://localhost:3000")
+app.listen(PORT, () => {
+  console.log("http://localhost:3000");
 });
-
--- util/database.js
+++ util/database.js
@@ -1,12 +1,15 @@
 import Database from "better-sqlite3";
 const db = new Database("./data/database.sqlite");
 db.prepare(
-    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email STRING, password STRING)"
+  "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email STRING, password STRING)"
 ).run();
 
 export const saveUser = (email, password) => {
-    db.prepare("INSERT INTO users (email, password) VALUES (?,?)").run(email,password);
+  db.prepare("INSERT INTO users (email, password) VALUES (?,?)").run(
+    email,
+    password
+  );
 };
 export const getUserByMail = (email) => {
-    return db.prepare("SELECT * FROM users WHERE email = ?").get(email);
-}
+  return db.prepare("SELECT * FROM users WHERE email = ?").get(email);
+};
